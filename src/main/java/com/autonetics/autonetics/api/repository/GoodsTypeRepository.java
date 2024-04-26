b70ab503c82fc6547a61e7099c6881fe5ee6e8c3
tellmant <petukhov.m05@gmail.com>
2024-04-27 02:32:07 +0300

    add: added GoodsTypeRepository

---
+++ src/main/java/com/autonetics/autonetics/api/repository/GoodsTypeRepository.java
@@ -0,0 +1,7 @@
+package com.autonetics.autonetics.api.repository;
+
+import com.autonetics.autonetics.api.model.entity.GoodsType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoodsTypeRepository extends JpaRepository<GoodsType, Long> {
+}
