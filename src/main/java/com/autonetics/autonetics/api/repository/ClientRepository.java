aa45d5135aa56dec38fd303938d407efe88311c6
tellmant <petukhov.m05@gmail.com>
2024-04-27 16:29:44 +0300

    fix: findByEmail optionally returns Client

--- src/main/java/com/autonetics/autonetics/api/repository/ClientRepository.java
+++ src/main/java/com/autonetics/autonetics/api/repository/ClientRepository.java
@@ -3,6 +3,8 @@ package com.autonetics.autonetics.api.repository;
 import com.autonetics.autonetics.api.model.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Client findByEmail(String email);
+    Optional<Client> findByEmail(String email);
 }
