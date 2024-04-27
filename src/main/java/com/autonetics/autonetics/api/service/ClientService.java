8492aaced0909c1b4bb4af1984e0464d768f8262
tellmant <petukhov.m05@gmail.com>
2024-04-27 16:30:07 +0300

    fix: ClientService implements UserDetailsService

--- src/main/java/com/autonetics/autonetics/api/service/ClientService.java
+++ src/main/java/com/autonetics/autonetics/api/service/ClientService.java
@@ -1,11 +1,12 @@
 package com.autonetics.autonetics.api.service;
 
 import com.autonetics.autonetics.api.model.entity.Client;
+import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface ClientService { //must extend UserDetailsService
+public interface ClientService extends UserDetailsService { //must extend UserDetailsService
     Client create(Client client);
     Client readById(long id);
     Client update(Client client);
