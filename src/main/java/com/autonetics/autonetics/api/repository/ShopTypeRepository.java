7d1b4edf38049d7ffd3ec82f0cbaacd4be524eca
Olexandr Mozil <sashamozil@gmail.com>
2024-04-27 22:04:00 +0300

    add: added ShopTypeRepository

---
+++ src/main/java/com/autonetics/autonetics/api/repository/ShopTypeRepository.java
@@ -0,0 +1,7 @@
+package com.autonetics.autonetics.api.repository;
+
+import com.autonetics.autonetics.api.model.entity.ShopType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShopTypeRepository extends JpaRepository<ShopType, Long> {
+}
