8626e908b2a1acae6bba7713a8b99d734f754894
Olexandr Mozil <sashamozil@gmail.com>
2024-04-28 15:32:39 +0300

    add: added DistrictMapper

---
+++ src/main/java/com/autonetics/autonetics/api/mapper/DistrictMapper.java
@@ -0,0 +1,24 @@
+package com.autonetics.autonetics.api.mapper;
+
+import com.autonetics.autonetics.api.mapper.RegionMapper;
+import com.autonetics.autonetics.api.model.entity.District;
+import com.autonetics.autonetics.api.model.request.NewDistrictRequest;
+import com.autonetics.autonetics.api.model.response.DistrictDto;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {RegionMapper.class})
+public interface DistrictMapper {
+    District toEntity(DistrictDto districtDto);
+
+    DistrictDto toDto(District district);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    District partialUpdate(DistrictDto districtDto, @MappingTarget District district);
+
+    District toEntity(NewDistrictRequest newDistrictRequest);
+
+    NewDistrictRequest toDto1(District district);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    District partialUpdate(NewDistrictRequest newDistrictRequest, @MappingTarget District district);
+}
