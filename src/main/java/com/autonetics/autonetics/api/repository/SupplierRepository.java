8508cb142f30201d126a6c832b3d1c8883e97b3e
tellmant <petukhov.m05@gmail.com>
2024-05-02 16:54:19 +0300

    add: added SupplierRepository

---
+++ src/main/java/com/autonetics/autonetics/api/repository/SupplierRepository.java
@@ -0,0 +1,7 @@
+package com.autonetics.autonetics.api.repository;
+
+import com.autonetics.autonetics.api.model.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+}
