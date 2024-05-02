31f0ba696776c50f5ff5dc9558b2b8265181c48f
tellmant <petukhov.m05@gmail.com>
2024-05-02 18:44:40 +0300

    fix: ignored mapping from long to entities in GoodsMapper

--- src/main/java/com/autonetics/autonetics/api/mapper/GoodsMapper.java
+++ src/main/java/com/autonetics/autonetics/api/mapper/GoodsMapper.java
@@ -1,25 +1,38 @@
 package com.autonetics.autonetics.api.mapper;
 
 import com.autonetics.autonetics.api.model.entity.Goods;
+import com.autonetics.autonetics.api.model.entity.Supplier;
 import com.autonetics.autonetics.api.model.request.NewGoods;
 import com.autonetics.autonetics.api.model.request.PatchGoods;
 import com.autonetics.autonetics.api.model.response.GoodsDto;
+import com.autonetics.autonetics.api.service.SupplierService;
+import jdk.jfr.Name;
 import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {CountryMapper.class, ClassMapper.class, SupplierMapper.class, GoodsTypeMapper.class})
 public interface GoodsMapper {
     Goods toEntity(GoodsDto goodsDto);
 
     GoodsDto toDto(Goods goods);
 
-    NewGoods toNewGoods(GoodsDto goodsDto);
-
-    PatchGoods toPatchGoods(GoodsDto goodsDto);
-
+    @Mapping(target = "supplierID", source = "supplierID", ignore = true)
+    @Mapping(target = "goodsTypeId", source = "goodsTypeId", ignore = true)
+    @Mapping(target = "countryID", source = "countryID", ignore = true)
+    @Mapping(target = "classID", source = "classID", ignore = true)
     Goods toEntity(NewGoods newGoods);
 
+
+    @Mapping(target = "supplierID", source = "supplierID", ignore = true)
+    @Mapping(target = "goodsTypeId", source = "goodsTypeId", ignore = true)
+    @Mapping(target = "countryID", source = "countryID", ignore = true)
+    @Mapping(target = "classID", source = "classID", ignore = true)
     Goods toEntity(PatchGoods patchGoods);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Goods partialUpdate(GoodsDto goodsDto, @MappingTarget Goods goods);
 }
+
