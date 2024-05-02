b9472f3dc00381a49a4ec380d5d9fe433468fd7d
Olexandr Mozil <sashamozil@gmail.com>
2024-05-02 20:17:25 +0300

    fix: simplified POST and PATCH methods in District API

--- src/main/java/com/autonetics/autonetics/api/model/request/NewDistrictRequest.java
+++ src/main/java/com/autonetics/autonetics/api/model/request/NewDistrictRequest.java
@@ -1,6 +1,5 @@
 package com.autonetics.autonetics.api.model.request;
 
-import com.autonetics.autonetics.api.model.response.RegionDto;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,7 +8,5 @@ import java.io.Serializable;
 /**
  * DTO for {@link com.autonetics.autonetics.api.model.entity.District}
  */
-public record NewDistrictRequest(@NotNull(message = "District name can't be null") @Size(max = 50) String name,
-                                 @NotNull(message = "Region can't be null") RegionDto region,
-                                 @NotNull(message = "District updated by can't be null") @Size(max = 50) String updatedBy) implements Serializable {
+public record NewDistrictRequest(@NotNull @Size(max = 50) String name, Integer regionId) implements Serializable {
 }
