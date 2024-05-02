8927f1d6c1a9cde3887d747e0f50938b2210c943
Olexandr Mozil <sashamozil@gmail.com>
2024-05-02 16:01:00 +0300

    add: added AddressMapper

---
+++ src/main/java/com/autonetics/autonetics/api/mapper/AddressMapper.java
@@ -0,0 +1,22 @@
+package com.autonetics.autonetics.api.mapper;
+
+import com.autonetics.autonetics.api.mapper.StreetMapper;
+import com.autonetics.autonetics.api.model.entity.Address;
+import com.autonetics.autonetics.api.model.request.NewAddressRequest;
+import com.autonetics.autonetics.api.model.response.AddressDto;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {StreetMapper.class})
+public interface AddressMapper {
+    Address toEntity(AddressDto addressDto);
+
+    AddressDto toDto(Address address);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Address partialUpdate(AddressDto addressDto, @MappingTarget Address address);
+
+    Address toEntity(NewAddressRequest newAddressRequest);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Address partialUpdate(NewAddressRequest newAddressRequest, @MappingTarget Address address);
+}
