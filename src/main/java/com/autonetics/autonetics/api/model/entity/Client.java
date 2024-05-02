5940a78ec31bc0ffcbf16b5257c9ca3082469eee
tellmant <petukhov.m05@gmail.com>
2024-05-02 18:45:08 +0300

    fix: using java.lang.Class class

--- src/main/java/com/autonetics/autonetics/api/model/entity/Client.java
+++ src/main/java/com/autonetics/autonetics/api/model/entity/Client.java
@@ -85,8 +85,8 @@ public class Client implements UserDetails {
     public final boolean equals(Object o) {
         if (this == o) return true;
         if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        java.lang.Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        java.lang.Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
         if (thisEffectiveClass != oEffectiveClass) return false;
         Client client = (Client) o;
         return getId() != null && Objects.equals(getId(), client.getId());
