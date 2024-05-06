f3f2d0fff78888e2ef7afd7a05a605d8f86b09bf
tellmant <petukhov.m05@gmail.com>
2024-05-06 18:57:29 +0300

    add: added DeliveryGoodsMapper

---
+++ src/main/java/com/autonetics/autonetics/api/mapper/DeliveryGoodsMapper.java
@@ -0,0 +1,20 @@
+package com.autonetics.autonetics.api.mapper;
+
+import com.autonetics.autonetics.api.model.entity.DeliveryGoods;
+import com.autonetics.autonetics.api.model.request.NewDeliveryGoods;
+import com.autonetics.autonetics.api.model.response.DeliveryGoodsDto;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface DeliveryGoodsMapper {
+    DeliveryGoods toEntity(DeliveryGoodsDto deliveryGoodsDto);
+
+    DeliveryGoodsDto toDto(DeliveryGoods deliveryGoods);
+
+    @Mapping(target = "goodsID", ignore = true)
+    @Mapping(target = "supplierID", ignore = true)
+    DeliveryGoods toEntity(NewDeliveryGoods newDeliveryGoods);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    DeliveryGoods partialUpdate(DeliveryGoodsDto deliveryGoodsDto, @MappingTarget DeliveryGoods deliveryGoods);
+}
