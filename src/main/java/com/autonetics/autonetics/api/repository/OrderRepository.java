9da7e8128a363942fd7f8a12542de3895fe43b12
tellmant <petukhov.m05@gmail.com>
2024-05-06 20:07:29 +0300

    add: added OrderRepository

---
+++ src/main/java/com/autonetics/autonetics/api/repository/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.autonetics.autonetics.api.repository;
+
+import com.autonetics.autonetics.api.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findByBarcode(String barcode);
+
+    Optional<Order> findByClientID_Email(String email);
+
+    Optional<List<Order>> findByNameContains(String name);
+}
