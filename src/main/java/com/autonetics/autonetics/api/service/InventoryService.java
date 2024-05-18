7f2ff9894913ef301de84693592228ccc1e80944
Olexandr Mozil <sashamozil@gmail.com>
2024-05-18 15:18:06 +0300

    add: added InventoryService and implemented in InventoryServiceImpl

---
+++ src/main/java/com/autonetics/autonetics/api/service/InventoryService.java
@@ -0,0 +1,16 @@
+package com.autonetics.autonetics.api.service;
+
+import com.autonetics.autonetics.api.model.entity.Goods;
+import com.autonetics.autonetics.api.model.entity.Inventory;
+
+import java.util.List;
+
+public interface InventoryService {
+    Inventory create(Inventory inventory);
+    Inventory readById(long id);
+    Inventory update(Inventory inventory);
+    void delete(long id);
+    List<Inventory> getAll();
+    List<Inventory> findAllGoodsByShopID_Id(Long shopId);
+    List<Inventory> findAllShopsByGoodsID_Id(Integer goodsId);
+}
