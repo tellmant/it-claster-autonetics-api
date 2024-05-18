45bcd51b1ec10caf56654f3559d5063b430d8df0
Olexandr Mozil <sashamozil@gmail.com>
2024-05-18 21:07:51 +0300

    add: added toString() methods

--- src/main/java/com/autonetics/autonetics/api/model/request/RequestGoodsAi.java
+++ src/main/java/com/autonetics/autonetics/api/model/request/RequestGoodsAi.java
@@ -1,5 +1,7 @@
 package com.autonetics.autonetics.api.model.request;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,4 +14,13 @@ import java.math.BigDecimal;
 public record RequestGoodsAi(Integer id, BigDecimal priceIn, BigDecimal priceOut, Float weight, @NotNull Integer rating,
                              @Size(max = 255) String name, @Size(max = 255) String description,
                              @Size(max = 255) String composition) implements Serializable {
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error while converting RequestGoodsAi object to JSON", e);
+        }
+    }
 }
--- src/main/java/com/autonetics/autonetics/api/model/response/WeatherDto.java
+++ src/main/java/com/autonetics/autonetics/api/model/response/WeatherDto.java
@@ -1,6 +1,7 @@
 package com.autonetics.autonetics.api.model.response;
 
-import lombok.ToString;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -11,4 +12,13 @@ import java.math.BigDecimal;
 public record WeatherDto(BigDecimal latitude, BigDecimal longitude, String city, String country,
                          double temperatureCelcius, double feelsLikeTemperatureCelcius, double windSpeed, int humidity,
                          double pressure, String weatherCondition, int isDay) implements Serializable {
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error while converting WeatherDto object to JSON", e);
+        }
+    }
 }
